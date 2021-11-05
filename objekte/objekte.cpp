00bb700775d88e0e38b54861d867dcc376ae5bde
Geyerlechner <michael.geyerlechner@liwest.at>
2021-11-05 08:56:44 +0100

    Wortwiederholungen nummerieren

--- objekte/objekte.cpp
+++ objekte/objekte.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+    int number_of_words = 0;
+    std::string previous = " ";
+    std::string current;
 
+    while( std::cin>>current )
+    {
+        ++number_of_words;
+        if ( previous == current ) 
+        {
+            std::cout << "Wort Nummer " << number_of_words 
+            << "ist eine Wiederholung: " << current << std::endl;
+        }
+    previous = current;
+    }
 }
