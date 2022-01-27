0b0a5ed34b5ddd85961f54a524dd45f861d837e0
Geyerlechner <michael.geyerlechner@liwest.at>
2022-01-27 16:49:31 +0100

    add for loop

--- for_loop_variants/for_loop_variants.cpp
+++ for_loop_variants/for_loop_variants.cpp
@@ -4,24 +4,29 @@
 
 int main()
 {
-	std::vector< int > v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	// Print ascii 
+	for (int i = 0; i < 256; i++)
+		std::cout << (char)i << " ";
 
+	
+	std::vector< int > v{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	
 	for( std::size_t i = 0; i < v.size(); ++i )
 		std::cout << v[ i ];
 	std::cout << std::endl;
-
+	
 	for( std::vector< int >::iterator iter = v.begin(); iter != v.end(); ++iter )
 		std::cout << *iter;
 	std::cout << std::endl;
-
+	
 	for( std::vector< int >::reverse_iterator iter = v.rbegin(); iter != v.rend(); ++iter )
 		std::cout << *iter;
 	std::cout << std::endl;
-
+	
 	for( auto value : v )
 		std::cout << value;
 	std::cout << std::endl;
-
+	
 	for( auto value : std::ranges::reverse_view( v ) )
 		std::cout << value;
 	std::cout << std::endl;
