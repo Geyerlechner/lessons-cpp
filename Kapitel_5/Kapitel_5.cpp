95a3eb2bae15afc6bc3d1744a4a57d35d0e2e24e
Geyerlechner <michael.geyerlechner@liwest.at>
2022-02-04 11:07:52 +0100

    Typenfehler

--- Kapitel_5/Kapitel_5.cpp
+++ Kapitel_5/Kapitel_5.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+int area(int number)
+{
+	return number;
+}
+
 int main()
 {
+
+	//Typefehler
+	// int x0 = arena(7); //Fehler: nicht deklariete Funktion
+	// int x1 = area(7); // Fehler: flasche Anzahl an Argumenten
+	// int x2 = area("seven", 2); // Fehler: erstes Argument hat den falschen Typ
+
+
 
 }
