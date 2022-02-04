a5fab0c9bc5429d77bbc158a3e783d8281a0e2ee
Geyerlechner <michael.geyerlechner@liwest.at>
2022-02-04 14:16:33 +0100

    try catch

--- Kapitel_5/Kapitel_5.cpp
+++ Kapitel_5/Kapitel_5.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include "std_lib_facilities.h"
 
+class Bad_area {}; //Typ, der speziell für das Melden von Fehlern aus area() definiert wurde
+
 int area(int length, int width)
 {
-	return length*width;
+	if(length <= 0 || width <= 0) throw Bad_area();
+
+	return length * width;
 }
 
 int framed_area(int x, int y) // berechnet die Fläche innerhalb eines Rahmens
@@ -59,10 +63,24 @@ void lesson_2()
 
 }
 
+
 int main()
 {
+	try {
+	
+		int x = -1;
+		int y = 2;
+		int z = 4;
 
+		int area1 = area( x, y );
+		int area2 = framed_area( 1, z );
+		int area3 = framed_area( y, z );
+		double ratio = area1/area3;
 
+	}
+	catch(Bad_area){
+		std::cout << "Hoppla! Ungueltige Argumente fuer area()\n";
+	}
 
 
 
