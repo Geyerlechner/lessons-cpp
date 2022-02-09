07e6a8af434ed50040a34b2fd3c1f78d39ecace2
Geyerlechner <michael.geyerlechner@liwest.at>
2022-02-09 11:18:10 +0100

    add calculator

--- Kapitel_6/Kapitel_6.cpp
+++ Kapitel_6/Kapitel_6.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include "../std_lib_facilities.h"
 
 int main()
 {
+	cout << "Geben Sie bitte einen Ausdruck ein (wir unterstuetzen + und -): ";
+	int lval = 0;
+	int rval = 0;
+	char op;
+	int res;
+
+	cin >> lval >> op >> rval;
+
+	if( op == '+' )
+		res = lval + rval;  // Addition
+	else if( op == '-' ) 
+		res = lval - rval; // Subtraktion
+
+	cout << "Ergebnis: " << res << '\n';
+
+	keep_window_open();
 
 }
