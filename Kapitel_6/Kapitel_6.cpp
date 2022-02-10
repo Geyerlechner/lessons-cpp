cc632b065c1d8bfc1330a6d0533a0d38f2fe2fea
Geyerlechner <michael.geyerlechner@liwest.at>
2022-02-10 11:13:28 +0100

    update main

--- Kapitel_6/Kapitel_6.cpp
+++ Kapitel_6/Kapitel_6.cpp
@@ -79,39 +79,21 @@ double expression()
 int main()
 {
 
-	//cout << "Geben Sie bitte einen Ausdruck ein (wir unterstuetzen +, -, * und /):";
-	//int lval = 0;
-	//int rval;
-	//char op;
-	//cin >> lval;
-	//
-	//if(!cin) error("Kein erster Operand");
-	//
-	//while( cin>>op ){
-	//
-	//	if(op != 'x') cin>>rval;
-	//	if(!cin) error("kein zweiter Operand");
-	//
-	//	switch(op){
-	//	case '+':
-	//		lval += rval;
-	//		break;
-	//	case '-':
-	//		lval -= rval;
-	//		break;
-	//	case '*':
-	//		lval *= rval;
-	//		break;
-	//	case '/':
-	//		lval /= rval;
-	//		break;
-	//	default: 
-	//		cout << "Ergebnis: " << lval << '\n';
-	//		keep_window_open();
-	//		return 0;
-	//	}
-	//}
-	//
-	//error("ungueltiger Ausdruck");
+	try {
+		while(cin)
+			cout << expression() << '\n';
+		keep_window_open();
+
+	}
+	catch(exception &e){
+		cerr << e.what() << endl;
+		keep_window_open();
+		return 1;
+	}
+	catch(...){
+		cerr << "Ausnahme \n";
+		keep_window_open();
+		return 2;
+	}
 
 }
